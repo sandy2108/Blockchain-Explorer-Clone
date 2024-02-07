e8b84a9fbca04d6eabf0f0434e0e430d36cec6ab
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-02-07 11:16:38 +0530

    BlockPage: Added the Index.jsx to the Block component

--- frontend/src/pages/Block/index.jsx
+++ frontend/src/pages/Block/index.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import BlockHash from '../../components/Block/BlockHash'
+import { useParams } from "react-router-dom";
+import { useFetchBlockQuery } from "../../features/api/apislice";
+import Loader from '../../utils/Loader';
+import BlockInformation from "../../components/Block/BlockInformation";
 
 const index = () => {
+    const { block } = useParams();
+    if (block.length != 8) {
+        return <p>Invalid Valid</p>
+    }
+    const { data, isLoading, isError, isFetching } = useFetchBlockQuery(block);
+
+    if (isLoading) {
+        return <Loader />;
+    }
+
+    if (isError) {
+        return <p>Error fetching Block data.</p>;
+    }
+
     return (
-        <div>index</div>
+        <div>
+            <BlockHash />
+            <BlockInformation data={data} />
+
+        </div>
     )
 }
 
