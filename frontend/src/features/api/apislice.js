69fe80d7dbaede304ffb36cd4991997b131ed853
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-02-10 18:18:28 +0530

    API: Updated the Api endpoint

--- frontend/src/features/api/apislice.js
+++ frontend/src/features/api/apislice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3001",
+        baseUrl: "https://blockchain-explorer-clone.vercel.app/",
         prepareHeaders: (headers, { getState }) => {
             headers.set("Cache-Control", "no-cache");
             return headers;
